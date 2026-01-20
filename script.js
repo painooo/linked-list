21f2894858128110ccaeff0135bd0e02a98c1e90
pyr <jzhangnv@gmail.com>
2026-01-19 23:02:08 -0500

    Added pop, at, head, size, prepend, and append functions to linkList

--- script.js
+++ script.js
@@ -2,6 +2,60 @@ class linkedList {
     constructor(){
         this.list = {};
     }
+    append(value){
+        if (this.list.nextNode == null){
+            this.prepend(value);
+        } else {
+            let next = this.list
+            while (next.nextNode.nextNode != null) {
+                next = next.nextNode;
+            }
+            next.nextNode = new Node(value, {});
+        }
+    }
+    prepend(value) {
+        this.list = new Node(value, this.list);
+    }
+    size(){
+        let count = 0;
+        let next = this.list;
+        while (next.nextNode != null) {
+            next = next.nextNode;
+            count++;
+        }
+        return count;
+    }
+    head(){
+        return this.list.value;
+    }
+    // tail(){ FIX THIS
+    //     let next = this.list;
+    //     while (next.nextNode != null) {
+    //         next = next.nextNode;
+    //     }
+    //     return next.value;
+    // }
+    at(index){
+        if (index == 0){
+            return this.head();
+        }
+        if (index == this.size()-1){
+            return this.tail();
+        }
+        let count = 1;
+        let next = this.list.nextNode;
+        while (next.nextNode != null) {
+            if (count == index) return next.value;
+            count++;
+            next = next.nextNode;
+        }
+        return next.value
+    }
+    pop(){
+        if (this.list.value == null) return undefined;
+        this.list = new Node(this.list.nextNode, {});
+        return this.list
+    }
 }
 class Node {
     constructor(value, nextNode){
@@ -9,3 +63,12 @@ class Node {
         this.nextNode = nextNode;
     }
 }
+const list = new linkedList();
+list.append('abc');
+list.append('abc2');
+list.append('abc3');
+console.log(list.list)
+console.log(list.size())
+//console.log(list.tail())
+console.log(list.at(1)) // abc2
+console.log(list.pop())
