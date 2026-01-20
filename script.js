c3c298188ac88e8363b5a7c2b3f1b89a7bd49372
pyr <jzhangnv@gmail.com>
2026-01-19 21:56:55 -0500

    Added linkedList & Node class

---
+++ script.js
@@ -0,0 +1,11 @@
+class linkedList {
+    constructor(){
+        this.list = {};
+    }
+}
+class Node {
+    constructor(value, nextNode){
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
