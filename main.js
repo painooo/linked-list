68d141943b6fe212a9ea96cc189bab53cd878e61
pyr <jzhangnv@gmail.com>
2026-01-24 21:25:12 -0500

    Updated name of linkedList file

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import {linkedList} from './script.js';
+import {linkedList} from './linkedList.js';
 const list = new linkedList();
 
-list.append("dog");
+list.append(['dog','key']);
 list.append("cat");
 list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
-console.log(list.toString())
+console.log(list.list)
